ee0d19045dc09fc711c4f09dcdeef3526f1c6c6a
Arulchelvam Sinthujan <115763555+Arulsinthu@users.noreply.github.com>
2022-12-29 16:00:49 +0530

    Update CurrencyConversionWs.java

--- src/com/sltc/soa/currency/ws/CurrencyConversionWs.java
+++ src/com/sltc/soa/currency/ws/CurrencyConversionWs.java
@@ -7,13 +7,6 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.xml.ws.Endpoint;
 
-/**
- * Created by IntelliJ IDEA.<br/>
- * User: Thilanka<br/>
- * Date: 12/20/2022<br/>
- * Time: 9:57 PM<br/>
- * To change this template use File | Settings | File Templates.
- */
 @WebService
 @SOAPBinding(style = SOAPBinding.Style.DOCUMENT)
 public class CurrencyConversionWs {
